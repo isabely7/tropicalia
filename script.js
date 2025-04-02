4a734d37247d7ef3818393b5d2947772bde3b0a7
isabely7 <166526171+isabely7@users.noreply.github.com>
2025-04-02 12:24:42 -0300

    02/04

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
+botaoDeAcessibilidade.addEventListener('click', function () {
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+});
+
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+const alternaContraste = document.getElementById ('alterna-contraste');
 
     let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
@@ -13,11 +23,10 @@ diminuiFonteBotao.addEventListener('click', function() {
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 });
+
+
+alternaContraste.addEventListener('click', function() {
+    document.body.classList.toggle('alto-contraste')
 });
 
-const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-const opcoesDeAcessibilidade = document.getElementById ('opcoes-acessibilidade');
-botaoDeAcessibilidade.addEventListener('click', function () {
-    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-})
+});
